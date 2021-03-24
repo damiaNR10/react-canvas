1a29e34af41159e1ea9b4cc3d7541207a664ddac
damiaNR10 <damianrog96@gmail.com>
2021-03-24 23:56:01 +0100

    Canvas in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class CanvasExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef();
+  }
+
+  draw = () => {
+    const ctx = this.canvas.current.getContext("2d");
+    const {size, rectanglesCount} = this.props;
+    ctx.clearRect(0, 0, size, size);
+    ctx.fillStyle = "rgba(0, 150, 0, 0.2)"
+    for(let i = 0; i < rectanglesCount; i++) {
+      const reactSize = (size * (i + 1.0)) / rectanglesCount;
+      ctx.fillRect(0, 0, reactSize, reactSize);
+    }
+  }
+
+  componentDidMount() {
+    this.draw();
+  }
+
+  render() {
+    const {size} = this.props;
+    return (
+      <canvas ref = {this.canvas} style = {{border: "1px solid gray"}} width = {size} height = {size} />
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Canvas!</h1>
+      <CanvasExample size = "300" rectanglesCount = {10}/>
     </div>
   );
 }
