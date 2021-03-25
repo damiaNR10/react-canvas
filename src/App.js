48ee4dee57c68d773c19d0c283b468045af939a1
damiaNR10 <damianrog96@gmail.com>
2021-03-26 00:33:26 +0100

    Final commit

--- src/App.js
+++ src/App.js
@@ -7,21 +7,47 @@ class CanvasExample extends React.Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
+    this.time = 0;
   }
 
   draw = () => {
     const ctx = this.canvas.current.getContext("2d");
     const {size, rectanglesCount} = this.props;
     ctx.clearRect(0, 0, size, size);
-    ctx.fillStyle = "rgba(0, 150, 0, 0.2)"
+    ctx.fillStyle = "rgba(0, 150, 0, 0.2)";
+    ctx.save();
+    ctx.translate(size/2, size/2);
+    const maxRectSize = size / Math.sqrt(2);
     for(let i = 0; i < rectanglesCount; i++) {
-      const reactSize = (size * (i + 1.0)) / rectanglesCount;
-      ctx.fillRect(0, 0, reactSize, reactSize);
+      const ratio = maxRectSize * (i + 1.0);
+      const rectSize = ratio / rectanglesCount;
+      const angle = ratio;
+      ctx.save();
+      ctx.rotate(angle + this.time);
+      ctx.translate(-rectSize / 2, -rectSize / 2);
+      ctx.fillRect(0, 0, rectSize, rectSize);
+      ctx.restore();
     }
+    ctx.restore();
+  }
+
+  update = () => {
+    this.time += 0.02;
+    this.draw();
+    this.animationRequestId = window.requestAnimationFrame(this.update);
   }
 
   componentDidMount() {
-    this.draw();
+    // this.draw();
+    this.animationRequestId = window.requestAnimationFrame(this.update);
+  }
+
+  componentDidUpdate() {
+    // this.draw();
+  }
+
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this.animationRequestId);
   }
 
   render() {
@@ -32,13 +58,33 @@ class CanvasExample extends React.Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Canvas!</h1>
-      <CanvasExample size = "300" rectanglesCount = {10}/>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    rectanglesCount: 10,
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Canvas!</h1>
+        <button onClick={() => {
+          //this.setState(prevState) => 
+          this.setState(prevState => ({
+            rectanglesCount: prevState.rectanglesCount + 1
+          }));
+        }}>+1</button>
+        <button onClick={() => {
+          //this.setState(prevState) => 
+          this.setState(prevState => ({
+            rectanglesCount: prevState.rectanglesCount - 1
+          }));
+        }}>-1</button>
+        <br />
+        <CanvasExample size = "300" rectanglesCount = {this.state.rectanglesCount}/>
+      </div>
+    );
+  }
 }
 
 export default App;
